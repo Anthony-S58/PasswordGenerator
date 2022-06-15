e7da89c46ce02cb1d2ba3cced2afb9081ea2fb0e
Anthony <tonysim01@hotmail.com>
2022-06-15 13:11:38 +0200

    addvariables

--- password.py
+++ password.py
@@ -1,3 +1,14 @@
 # Password Generator Test
 
+#import random for randomize all
 import random
+
+#set variables for password with lower case, upper case, numbers and symbols
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "@#$%&*-_\/"
+
+
+
